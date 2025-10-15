92bbfa113e18a1599bee16eddc73e3b592ab130c
iam-siva25 <sivabalasubramanian2502@gmail.com>
2025-10-15 18:02:06 +0530

    cors update commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const protectedRoutes = require('./routes/protected')
 
 app.use(cors({ 
     // 🔑 FIX: Specify the exact frontend URL instead of the wildcard
-    origin: 'http://localhost:3000', 
+    origin: 'https://authentication-xi-seven.vercel.app/', 
     
     // This must remain true to send/receive cookies
     credentials: true 
